5d762ac0879a504fde9ebfff29ee945019419097
PJ Frias <peejfrias@gmail.com>
2017-11-03 23:38:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*(100-1+1)+1)
+  var additem = {[item]: [price]}
+  console.log(`${item}` has been added to your cart.`)
 }
 
 function viewCart() {
