c3c2f56e36ef2693243eea8c13e188af4b883f30
PJ Frias <peejfrias@gmail.com>
2017-11-04 00:33:52 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -3,3 +3,11 @@ console.log(item)
 var cart = { [item]: Math.floor(Math.random()*(100)+1) }
 console.log(cart)
 console.log(`${item} has been added to your cart.`)
+
+basket = []
+function addtobasket(item) {
+  basket.push(item)
+  console.log(basket)
+}
+
+addtobasket("pear")
