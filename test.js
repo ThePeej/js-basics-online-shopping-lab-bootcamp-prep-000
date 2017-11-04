9615777f806dfa377271aaec3b7863b1398e7125
PJ Frias <peejfrias@gmail.com>
2017-11-04 03:55:36 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -43,9 +43,9 @@ console.log('~~~ viewCart testing ~~~')
 var n = cart.length
 console.log(n)
 
-///for ( let i = 0; i < cart.length; i++) {
+for ( let i = 0; i < cart.length; i++) {
 console.log(cart)
-console.log(cart[0])
-console.log(Object.keys(cart[1]))
-console.log(Object.keys(cart))
-///}
+console.log(cart[i])
+console.log(Object.keys(cart[i]))
+console.log(Object.keys(i))
+}
