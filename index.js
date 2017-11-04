f20940a46a76906fac0dd191ee6e150c477aa698
PJ Frias <peejfrias@gmail.com>
2017-11-04 12:43:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,11 +51,13 @@ function placeOrder(cardNumber) {
   if (cardNumber = NaN) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
+/*
     var totalcost = 0
     for ( let i = 0; i < cart.length; i++ ) {
       totalcost = totalcost + cart[i][Object.keys(cart[i])[0]]
     }
-    console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
+    */
+    console.log(`Your total cost is $${totalcost}, which will be charged to the card ${cardNumber}.`)
     return cart = []
   }
 }
