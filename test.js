1bde120b159101b87aebc77d24c224aac267f978
PJ Frias <peejfrias@gmail.com>
2017-11-04 00:22:05 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -1,33 +1,4 @@
-var cart = [];
-
-function getCart() {
- return cart;
-}
-
-function setCart(c) {
-  cart = c;
-  return cart;
-}
-
-function addToCart(item) {
-  var cart = { [item]: Math.floor(Math.random()*(100)+1) }
-  console.log(item)
-  console.log(`${item} has been added to your cart.`)
-  return cart
-}
-
-function viewCart() {
-  // write your code here
-}
-
-function total() {
-  // write your code here
-}
-
-function removeFromCart(item) {
-  // write your code here
-}
-
-function placeOrder(cardNumber) {
-  // write your code here
-}
+item = "apple"
+console.log(item)
+var cart = { [item]: Math.floor(Math.random()*(100)+1) }
+console.log(cart)
