7ea992c9803ae969a83b05a050736197fc27fe31
PJ Frias <peejfrias@gmail.com>
2017-11-04 12:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardnumber = NaN) {
+  if (cardNumber = NaN) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
