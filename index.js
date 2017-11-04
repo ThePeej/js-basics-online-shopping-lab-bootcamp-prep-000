b06c5f8378a4d036ddb6f1e85c7e14120c954619
PJ Frias <peejfrias@gmail.com>
2017-11-04 12:22:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,5 +48,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (isNan(cardnumber)) {
+    console.log("Sorry, we don't have a credit card on file for you.")
+  }
+  
 }
