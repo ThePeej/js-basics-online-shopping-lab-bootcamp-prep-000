b089d093228f6e4ea17d3924a11ee5eb413d79db
PJ Frias <peejfrias@gmail.com>
2017-11-04 15:16:25 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -60,7 +60,7 @@ console.log()
 */
 addToCart("Milk")
 addToCart("Cereal")
-///addToCart("Steak")
+addToCart("Steak")
 
 function viewCart() {
   var n = cart.length
@@ -75,7 +75,7 @@ function viewCart() {
       var food = Object.keys(cart[i])[0]
       var cost = cart[i][Object.keys(cart[i])[0]]
       var inCart = []
-      inCart.push(` ${food} at $${cost},`)
+      inCart.push(` Food at $cost,`)
     }
     console.log(inCart)
     console.log(`In your cart, you have${inCart}.`)
