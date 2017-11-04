0864c9b9ca34c6dd2b5c6890b31063fb06fc3733
PJ Frias <peejfrias@gmail.com>
2017-11-04 12:49:42 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -76,7 +76,7 @@ console.log('~~~ total cost testing ~~~')
 function placeOrder(cardNumber) {
 
   console.log(cardNumber)
-  if (cardNumber) {
+  if (isNaN(cardNumber)) {
     console.log("Sorry, we don't have a credit card on file for you.")
   } else {
     console.log(`Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`)
