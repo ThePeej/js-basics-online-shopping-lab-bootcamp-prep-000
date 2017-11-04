83baec20bc520274ebc96148e252e032d0ed1548
PJ Frias <peejfrias@gmail.com>
2017-11-04 13:40:27 +0000

    automatically backed up by learn

--- test.js
+++ test.js
@@ -91,6 +91,7 @@ console.log('~~~ remove item testing ~~~')
 console.log(cart)
 function removeFromCart(item) {
   for ( let i = 0; i < cart.length; i++) {
+    console.log(cart[i])
     if (cart[i].hasOwnProperty(item)) {
       console.log('yeah boi')
       delete cart.item
